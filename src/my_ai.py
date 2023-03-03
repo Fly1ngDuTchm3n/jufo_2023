89fd24ffb76ae0ad8e405fd244f17ab170cdf13c
Fly1ngDuTchm3n <88061217+Fly1ngDuTchm3n@users.noreply.github.com>
2023-03-03 22:03:36 +0100

    mad conf_mat optional

--- src/my_ai.py
+++ src/my_ai.py
@@ -3,11 +3,10 @@ from matplotlib import pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.ensemble import RandomForestClassifier
-from sklearn import tree
 from sklearn.metrics import ConfusionMatrixDisplay
 
 
-def build_ai(data, labels):
+def build_ai(data, labels, print_conf_mat=False):
     input_train, input_test, labels_train, labels_test = train_test_split(
         data, labels, random_state=1, test_size=0.2
     )
@@ -19,16 +18,15 @@ def build_ai(data, labels):
 
     model.fit(input_train, labels_train)
 
-    # disp = ConfusionMatrixDisplay.from_estimator(
-    #     model,
-    #     input_test,
-    #     labels_test,
-    #     normalize="true",
-    # )
+    if print_conf_mat:
+        disp = ConfusionMatrixDisplay.from_estimator(
+            model,
+            input_test,
+            labels_test,
+            normalize="true",
+        )
 
-    # plt.show()
-    # tree.plot_tree(model)
-    # plt.show()
+        plt.show()
 
     # print the accuracy of the model
     return model.score(input_test, labels_test)
