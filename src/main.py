4822439fa2ad8842bd9bde5b49e2db206349ec2a
Fly1ngDuTchm3n <88061217+Fly1ngDuTchm3n@users.noreply.github.com>
2023-03-03 22:00:43 +0100

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,8 +1,8 @@
-from spikeclustering import normalizing_data, normed_dataframe_to_list, spikes_per_cluster, values_per_spike
+from spikeclustering import normalizing_data, hand_picked_algorithm
 from my_ai import build_ai
-from src.fourier import fft_check_for_asp
+from fourier import replace_with_fft, hand_picked_fft_algorithm
 import pandas as pd
-
+import numpy as np
 
 oldAsp = pd.read_csv("datasets/ASPHALT_06.02.CSV")
 oldAsp.drop(["ADC_filtered", "current", "charging_time", "time"], axis=1)
@@ -12,18 +12,25 @@ oldGra.drop(["ADC_filtered", "current", "charging_time", "time"], axis=1)
 aspDs = normalizing_data(oldAsp)
 graDs = normalizing_data(oldGra)
 
+four_mat_asp = replace_with_fft(aspDs)
+four_mat_gra = replace_with_fft(graDs)
 
-four_asp = normed_dataframe_to_list(aspDs)
-four_gra = normed_dataframe_to_list(graDs)
+print(
+    f"accuracy four total = {1 - (hand_picked_fft_algorithm(four_mat_asp, isAsp=True) + hand_picked_fft_algorithm(four_mat_gra, isAsp=False))/(len(four_mat_asp) + len(four_mat_gra))}"
+)
+print(
+    f"accuracy handy total = {1 - (hand_picked_algorithm(aspDs, isAsp=True) + hand_picked_algorithm(graDs, isAsp=False))/(len(four_mat_asp) + len(four_mat_gra))}"
+)
+
+aspLab = np.full((len(aspDs)), "Asphalt")
+graLab = np.full((len(graDs)), "Kies")
+
+totalSet = np.concatenate((aspDs, graDs))
+totalSet2 = np.concatenate((four_mat_asp, four_mat_gra))
+totalLab = np.concatenate((aspLab, graLab))
 
 
-aspLab = pd.Series(["Asp"] * len(aspDs))
-graLab = pd.Series(["Gra"] * len(graDs))
-
-totalSet = [aspDs, graDs]
-totalSet = pd.concat(totalSet, ignore_index=True)
-totalLab = [aspLab, graLab]
-totalLab = pd.concat(totalLab, ignore_index=True)
-
-
-build_ai(totalSet, totalLab)
+print(
+    f"Genauigkeit maschinelles lernen auf normierten Daten: {build_ai(totalSet, totalLab)}"
+)
+print(f"Genauigkeit fft mit maschinellem lernen: {build_ai(totalSet2, totalLab)}")
